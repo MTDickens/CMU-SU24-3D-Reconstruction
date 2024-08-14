0912f3d59bfec89327744d5ddbe87135db8e1f17
mtdickens <mtdickens1998@gmail.com>
2024-08-14 00:23:22 -0400

    Update sparse construction pipeline

--- python/test_temple_coords.py
+++ python/test_temple_coords.py
@@ -3,13 +3,13 @@ import helper as hlp
 import skimage.io as io
 import submission as sub
 import matplotlib.pyplot as plt
-import cv2 as cv
+import cv2
 
 # 1. Load the two temple images and the points from data/some_corresp.npz
 
 #load images
-I1 = cv.cvtColor(cv.imread('../data/im1.png'), cv.COLOR_BGR2GRAY).astype(np.float32)
-I2 = cv.cvtColor(cv.imread('../data/im2.png'), cv.COLOR_BGR2GRAY).astype(np.float32)
+I1 = cv2.cvtColor(cv2.imread('../data/im1.png'), cv2.COLOR_BGR2GRAY).astype(np.float32)
+I2 = cv2.cvtColor(cv2.imread('../data/im2.png'), cv2.COLOR_BGR2GRAY).astype(np.float32)
 
 # intrinsics = np.load('../data/intrinsics.npz')
 # K1, K2 = intrinsics['K1'], intrinsics['K2']
@@ -33,12 +33,15 @@ temple_pts2 = sub.epipolar_correspondences(I1, I2, F, temple_pts1)
 
 # 5. Compute the camera projection matrix P1
 intrinsics = np.load('../data/intrinsics.npz')
-#获取相机内参
+
+## 获取相机内参
 K1, K2 = intrinsics['K1'], intrinsics['K2']
-#相机1的外参（单位矩阵，假设没有旋转和平移）
+
+## 相机1的外参（单位矩阵，假设没有旋转和平移）
 M1 = np.hstack((np.eye(3), np.zeros((3, 1))))
 P1 = K1 @ M1
-#计算本质矩阵
+
+## 计算本质矩阵
 E = sub.essential_matrix(F, K1, K2)
 
 # 6. Use camera2 to get 4 camera projection matrices P2
@@ -58,24 +61,27 @@ pts3d3 = sub.triangulate(P1, temple_pts1, P2s[:, :, 3], temple_pts2)
 
 # 8. Figure out the correct P2
 M2,pts3d = sub.find_correct_P2(M1, temple_pts1, M2s, temple_pts2, K1, K2)
-print("M2:))))))))))))))))))")
-print(M2)
-print("pts3d:))))))))))))))))))")
-print(pts3d)
+
+## Calculate the reconstruction loss
+reprojection = np.column_stack((pts3d, np.ones(pts3d.shape[0]))) @ P1.T
+reprojection = reprojection[:,0:2] / reprojection[:, 2].reshape(-1, 1)
+print(f"Reprojection Loss: {np.sum(np.abs(reprojection - temple_pts1))}")
 
 # 9. Scatter plot the correct 3D points
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(pts3d_cameraP1[0], pts3d_cameraP1[1], pts3d_cameraP1[2], c='r', marker='o')
-# ax.scatter(pts3d_cameraP2[0], pts3d_cameraP2[1], pts3d_cameraP2[2], c='b', marker='o')
+# ax = fig.add_subplot(131, projection='3d')
+# bx = fig.add_subplot(132)
+# cx = fig.add_subplot(133)
+# bx.scatter(temple_pts1[:, 0], temple_pts1[:, 1], c='r', marker='o')
+# cx.scatter(reprojection[:, 0], reprojection[:, 1], c='r', marker='o')
 ax.scatter(pts3d[:, 0], pts3d[:, 1], pts3d[:, 2], c='r', marker='o')
-# ax.scatter(pts3d0[:, 0], pts3d0[:, 1], pts3d0[:, 2], c='r', marker='o')
-# ax.scatter(pts3d1[:, 0], pts3d1[:, 1], pts3d1[:, 2], c='r', marker='o')
-# ax.scatter(pts3d2[:, 0], pts3d2[:, 1], pts3d2[:, 2], c='r', marker='o')
-# ax.scatter(pts3d3[:, 0], pts3d3[:, 1], pts3d3[:, 2], c='b', marker='o')
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
+
+ax.set_aspect('equal') # 注意将所有的 axis 的缩放程度设置成一样的，这样才不会造成点云的“扭曲”
+
 plt.show()
 
 # 10. Save the computed extrinsic parameters (R1,R2,t1,t2) to data/extrinsics.npz
